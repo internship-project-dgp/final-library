69449aed557580a13defd4e3e136512f7e3a9fdd
GIRIRAJ KUMAWAT <73705085+Gkraftaar123@users.noreply.github.com>
2022-06-28 11:48:39 +0530

    Add files via upload
    first

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGO_URI:
+        "mongodb+srv://Giriraj:Giriraj123@cluster0.vz1l7.mongodb.net/Library?retryWrites=true&w=majority",
+    JWT_SECRET: "GIRIRAJKUMAWAT698",
+};
